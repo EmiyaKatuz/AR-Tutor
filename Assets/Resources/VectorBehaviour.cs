5ef903d56fbe3db3537a97c9cf6b33f031dd57f7
camcamcamcamcam <eboysix@hotmail.co.nz>
2024-09-15 18:32:53 +1200

    My own objects and virtual vectors

---
+++ Assets/Resources/VectorBehaviour.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class VectorBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    int color = 0;
+
+    KeyCode[][] keys = { 
+        new KeyCode[]{ KeyCode.W, KeyCode.S, KeyCode.A, KeyCode.D, KeyCode.C, KeyCode.V },
+        new KeyCode[]{ KeyCode.I, KeyCode.K, KeyCode.J, KeyCode.L, KeyCode.M, KeyCode.N },
+        new KeyCode[]{ KeyCode.UpArrow, KeyCode.DownArrow, KeyCode.LeftArrow, KeyCode.RightArrow, KeyCode.Space, KeyCode.Slash } 
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(keys[color][0]))
+        {
+            transform.Rotate(0, 1, 0);
+        }
+        if (Input.GetKey(keys[color][1]))
+        {
+            transform.Rotate(0, -1, 0);
+        }
+        if (Input.GetKey(keys[color][2]))
+        {
+            transform.Rotate(1, 0, 0);
+        }
+        if (Input.GetKey(keys[color][3]))
+        {
+            transform.Rotate(-1, 0, 0);
+        }
+        if (Input.GetKey(keys[color][4]))
+        {
+            transform.Translate(0, 0, 0.1f);
+        }
+        if (Input.GetKey(keys[color][5]))
+        {
+            transform.Translate(0, 0, -0.1f);
+        }
+    }
+}
